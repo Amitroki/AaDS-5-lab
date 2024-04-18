5310f1b0afbb2771d02a4b053db6bf7314ef0dd1
Алексей Русяев <RusyaevAA@outlook.com>
2024-04-19 01:08:23 +0400

    added a destructor, copy constructor and a function for calculating a hash

--- class/hash_table.cpp
+++ class/hash_table.cpp
@@ -5,54 +5,102 @@
 using namespace std;
 
 namespace lab_5 {
+
+	double fractional_part(double num) {
+		return num - floor(num);
+	}
+
+	template <typename K>
+	int get_hash(K key, size_t sz) {
+		int a = 1636787;
+		int w = 64;
+		int result = (sz * (fractional_part(a * key / (w * 1.0)))) / 1;
+		return result;
+	}
+
 	template <typename K, typename V>
-	class my_pair : public pair<K, V> {
+	class Pair : public pair<K, V> {
 	private:
-		bool _init;
+
 		bool _del;
+
 	public:
-		my_pair() : pair<K, V>() {
-			_init = false;
+		Pair() : pair<K, V>() {
 			_del = false;
 		}
-		my_pair(K key, V value) : pair<K, V>(key, value) {
-			_init = false;
+		Pair(K key, V value) : pair<K, V>(key, value) {
 			_del = false;
-		}
-		void set_init() {
-			_init = true;
-		}
-		bool get_init() {
-			return _init;
-		}
-		bool get_del() {
-			return _del;
-		}
-		bool is_init() {
-			return _init;
 		}
 		bool is_del() {
 			return _del;
 		}
 		void del() {
-			_init = false;
 			_del = true;
 		}
 	};
 	template<typename K, typename V>
-	class hash_table {
+	class HashTable {
 	private:
 		size_t _size;
 		size_t _filled;
-		vector<my_pair<K, V>> _container;
+		vector<Pair<K, V>> _container;
+
+		void copy(const HashTable& other) {
+			_size = other._size;
+			_filled = other._filled;
+			_container = other._container;
+		}
+
+		void clear() {
+			_container.clear();
+			_size = 0;
+			_filled = 0;
+		}
+			
 	public:
-		hash_table(): _container(10), _size(10), _filled(0) {}
-		hash_table(size_t size) {
-			vector<my_pair<K, V>> temp(size, {0, 0});
+
+		HashTable(): _container(10), _size(10), _filled(0) {}
+		HashTable(size_t size) {
+			vector<Pair<K, V>> temp(size, {0, 0});
 			_container = temp;
 			_size = size;
 			_filled = 0;
 		}
+
+		HashTable(const HashTable& other) {
+			copy(other);
+		}
+
+		~HashTable() {
+			this->clear();
+		}
+
+		size_t get_size() const {
+			return _size;
+		}
+
+		HashTable operator=(const HashTable& other) {
+			auto newTable(other);
+			return newTable;
+		}
+
+		V* search(K key) {
+			size_t function = get_hash(key, this->get_size());
+			while (function < this->get_size()) {
+				if (_container[function].first == key && !_container[function].is_del()) {
+					return &_container[function].second;
+				}
+				function++;
+			}
+			return nullptr;
+		}
+
+		/*void insert(K key, V value) {
+			if (!search(key)) return;
+			Pair<K, V> new_element(key, value);
+			_container[get_hash(key, this->get_size())] = new_element;
+		}*/
+
 		void print() {
 			for (const auto& element : _container) {
 				cout << "< key: " << element.first << "> < value : " << element.second << endl;
--- tests/table_tests.cc
+++ tests/table_tests.cc
@@ -5,16 +5,36 @@
 using namespace lab_5;
 using namespace std;
 
-TEST(Pair, Creating) {
-	my_pair<int, int> a = {4, 2};
-	cout << a.get_del() << endl;
-	cout << a.get_init() << endl;
-	a.set_init();
-	cout << a.get_init() << endl;
+TEST(PairTest, Creating) {
+	Pair<int, int> a = {4, 2};
+	cout << a.is_del() << endl;
 	a.del();
-	cout << a.get_del() << endl;
+	cout << a.is_del() << endl;
 }
-TEST(HashTable, PrintHashTable) {
-	hash_table<int, int> a(10);
+TEST(HashTableTest, PrintHashTable) {
+	HashTable<int, int> a(10);
 	a.print();
 }
+TEST(HashTableTest, CopyHashTable) {
+	HashTable<int, int> a(10);
+	HashTable<int, int> b(a);
+	b.print();
+}
+TEST(HashTableTest, OperatorHashTable) {
+	HashTable<int, int> a(10);
+	HashTable<int, int> b = a;
+	b.print();
+}
+TEST(HashTableTest, SearchElement1) {
+	HashTable<int, int> a(10);
+	int *b = a.search(10);
+	int* c = a.search(0);
+	EXPECT_EQ(nullptr, b);
+}
+
+//TEST(HashTable, Insert) {
+//	HashTable<int, int> a(10);
+//	a.insert(111, 905);
+//	a.insert(109, 703);
+//	a.print();
+//}
